4a67a2c8ff40fbbcdab26206de186a2dbae093c1
stoerfisch <104793034+stoerfisch@users.noreply.github.com>
2022-11-30 10:47:50 +0100

    Add files via upload

--- RMN/gui.py
+++ RMN/gui.py
@@ -1,9 +1,21 @@
 import PySimpleGUI as sg
 from PIL import Image
 import io
+import json
+import random
+import os
+
+#loading json file
+with open('output/mapping.json', 'r') as file:
+    obj = json.load(file)
+
+print(obj)
+
 
 file_types = [("JPEG (*.jpg)", "*.jpg"),
               ("All files (*.*)", "*.*")]
+
+emo_dic = {'-ANGRY-': 'angry', '-DISGUST-': 'disgust', '-FEAR-': 'fear', '-HAPPY-': 'happy', '-NEUTRAL-': 'neutral','-SAD-':'sad', '-SURPRISED-': 'surprised'}
 
 # functions handling windows
 
@@ -26,18 +38,36 @@ def gamemodes_f():
 
     gamemodes_win.close()
 
-def emotionmode_f():
 
-    image_path = sg.popup_get_file('Open', no_window=True)
-    image = Image.open(image_path)
 
-    #handling different formats than PNG
+def random_image():
+    input_path = os.getcwd() + '\input'
+    image_file = random.choice(os.listdir(input_path))
+    # image_path = sg.popup_get_file('Open', no_window=True)
+    image = Image.open(input_path + '\\' + image_file)
+
+    #image size
+    image.thumbnail((400, 400))
+
+    # handling different formats than PNG
     bio = io.BytesIO()
     # Actually store the image in memory in binary
     image.save(bio, format="PNG")
 
-    data = bio.getvalue()
+    return bio.getvalue(), image_file
 
+def check_emotion(image_file, event):
+    emo_label = obj[image_file][0]['emo_label']
+    if emo_label == emo_dic[event]:
+        return True
+    return False
+
+
+
+def emotionmode_f():
+    shown_images = []
+    image, image_name = random_image()
+    shown_images.append(image_name)
 
     emotion_col = sg.Column([
         [sg.Button('wütend', key='-ANGRY-'), sg.Button('ekelerfüllt', key='-DISGUST-'), sg.Button('ängstlich', key='-FEAR-')],
@@ -45,18 +75,38 @@ def emotionmode_f():
         [sg.Button('traurig', key='-SAD-'), sg.Button('überrascht', key='-SURPRISED-')],
     ])
 
-    image_col = sg.Column([[sg.Image(data, key = '-IMAGE-')]])
+    image_col = sg.Column([[sg.Image(image, key = '-IMAGE-')]])
     emotion_lay = [[sg.Text("Welche Emotion wird hier gezegt?")],
                    [emotion_col, image_col],
-                   [sg.Button('Ok'), sg.Button('Quit')]]
+                   [sg.Text(visible = False, enable_events=True, key = '-VALID-'), sg.Button('Nächstes', key = '-NEXT-') ,sg.Button('Beenden', key = '-QUIT-')]]
 
-    original = Image.open(image_path)
+    #original = Image.open(image_path)
     emotionmode_win = sg.Window('Emotionszuordnung', emotion_lay)
+
 
     while True:
         event, values = emotionmode_win.read(timeout = 50)
-        if event in (None, 'Quit'):
+        #todo: function to load picture
+        if event in (None, '-QUIT-'):
             break
+        elif event in ('-ANGRY-','-DISGUST-','-FEAR-','-HAPPY-','-NEUTRAL-','-SAD-','-SURPRISED-'):
+            if check_emotion(image_name,event):
+                emotionmode_win['-VALID-'].update(visible=True)
+                emotionmode_win['-VALID-'].update('Richtig')
+            else:
+                emotionmode_win['-VALID-'].update(visible=True)
+                emotionmode_win['-VALID-'].update('Falsch')
+        elif event == '-NEXT-':
+            emotionmode_win['-VALID-'].update(visible=False)
+
+            #todo: print that all images where seen
+            while image_name in shown_images:
+                image, image_name = random_image()
+
+            emotionmode_win['-IMAGE-'].update(image)
+            shown_images.append(image_name)
+
+
 
     emotionmode_win.close()
 
@@ -101,3 +151,6 @@ while True:
 
 # Finish up by removing from the screen
 welc_win.close()
+
+
+#todo: Köpfe ausschneiden, wie mit Gruppenbilder umgehen?
