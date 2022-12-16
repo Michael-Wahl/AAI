efc0a2a933a1b33ebdf2d7d1e4bcb815ba30b1fd
Michael Wahl <M.Wahl@campus.lmu.de>
2022-12-16 18:41:24 +0100

    added backend + webcam component

--- RMN/backend/app.py
+++ RMN/backend/app.py
@@ -1,7 +1,32 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import os
+import base64
+import uuid
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/image-upload', methods=['POST'])
+def save_image():
+    # Check if the request is JSON and has the 'image' key
+    if request.is_json and 'image' in request.get_json():
+        # Decode the image data and save it to a file
+        image_data = request.get_json()['image']
+        image_data = image_data[image_data.index(',') + 1:]
+        image_data = base64.b64decode(image_data)
+
+        # Create a subfolder in the directory to store the images
+        subfolder = 'images'
+        if not os.path.exists(subfolder):
+            os.makedirs(subfolder)
+        
+        # Saving the image with a unique id as filename
+        filename = f'{uuid.uuid4()}.jpg'
+        filepath = os.path.join(subfolder, filename)
+        
+        with open(filepath, 'wb') as f:
+            f.write(image_data)
+        return f'{filename}', 200
+    else:
+        return 'Invalid request', 400
--- RMN/frontend/autismapp/src/components/WebcamUploader.vue
+++ RMN/frontend/autismapp/src/components/WebcamUploader.vue
@@ -2,10 +2,15 @@
   <div>
     <video ref="video" width="640" height="480"></video>
     <canvas ref="canvas" width="640" height="480"></canvas>
+    <br>
+    <button @click="startSendingImages">Start Sending Images</button>
+    <button @click="stopSendingImages">Stop Sending Images</button>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   data() {
     return {
@@ -13,6 +18,7 @@ export default {
       video: null,
       canvas: null,
       context: null,
+      message: ""
     };
   },
   mounted() {
@@ -30,11 +36,29 @@ export default {
       .catch(err => {
         console.error("Error: ", err);
       });
+  },
+  methods: {
+    startSendingImages() {
+      // Start taking pictures every 1 second
+      this.interval = setInterval(() => {
+        this.context.drawImage(this.video, 0, 0, 640, 480);
 
-    // Start taking pictures every 1 second
-    this.interval = setInterval(() => {
-      this.context.drawImage(this.video, 0, 0, 640, 480);
-    }, 1000);
+        // Convert the canvas to a data URL and send to server using axios
+        const imageData = this.canvas.toDataURL('image/png');
+        axios.post('http://127.0.0.1:5000/image-upload', { image: imageData }, 
+        { headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' } })
+          .then(response => {
+            this.message = response.data
+            console.log(this.message);
+          })
+          .catch(error => {
+            console.error('Error sending image to server: ', error);
+          });
+      }, 1000);
+    },
+    stopSendingImages() {
+      clearInterval(this.interval);
+    }
   },
   beforeUnmount() {
     clearInterval(this.interval);
