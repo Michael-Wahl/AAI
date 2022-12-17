06aaeff14f2665964f3fd348a34c43be42271766
Michael Wahl <M.Wahl@campus.lmu.de>
2022-12-17 11:58:45 +0100

    added Readme + output

---
+++ RMN/backend/README.md
@@ -0,0 +1,13 @@
+# BACKEND
+
+## To run the backend, cd into RMN/backend
+
+## The first time, run 
+```
+pip install -r requirements.txt
+```
+
+## To start the backend server, run
+```
+flask --app app --debug run --port=5000
+```
--- RMN/frontend/autismapp/src/components/ClassifyButton.vue
+++ RMN/frontend/autismapp/src/components/ClassifyButton.vue
@@ -1,15 +1,22 @@
 <template>
     <button @click="handleClick">Classify all uploaded images</button>
+    <p>{{ data }}</p>
   </template>
   
   <script>
   import axios from 'axios';
   
   export default {
+    data() {
+    return {
+      data: null
+    }
+  },
     methods: {
       async handleClick() {
         try {
           const response = await axios.get('http://127.0.0.1:5000/classify');
+          this.data = response.data
           console.log(response.data);
         } catch (error) {
           console.error(error);
